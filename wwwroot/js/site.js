ec8d2d8b782fa41d7e9cc2212eec776571068d99
William Goode <wgoode3@gmail.com>
2020-05-22 22:13:34 -0400

    styling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(".input-validation-error").each(function(){
+    $(this).addClass("is-invalid");
+});
